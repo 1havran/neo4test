347a529c191406a1cf78d91389c9ebe780395cea
hvr <havran.init0@gmail.com>
2018-05-29 23:14:02 +0200

    set tabstop=4

--- 01_generate_ipv4_nodes.py
+++ 01_generate_ipv4_nodes.py
@@ -4,16 +4,16 @@ import os
 from multiprocessing import cpu_count, Pool
 
 def generate_ipv4(low_range,high_range):
-	f = open('file-' + str(low_range) + '_' + str(high_range) + ".csv", "a")	
-	f.write('ip_addr:ID,ip_num' + "\n")
-	for a in range(low_range,high_range):
-		for b in range (0,256):
-			for c in range(0,256):
-				for d in range(0,256):
-					ip_addr = "%s.%s.%s.%s" % (a, b, c, d)
-					ip_num = a*256**3 + b*256**2 + c*256 + d
-					f.write(ip_addr + "," + str(ip_num) + "\n")
-	f.close()
+    f = open('file-' + str(low_range) + '_' + str(high_range) + ".csv", "a")    
+    f.write('ip_addr:ID,ip_num' + "\n")
+    for a in range(low_range,high_range):
+        for b in range (0,256):
+            for c in range(0,256):
+                for d in range(0,256):
+                    ip_addr = "%s.%s.%s.%s" % (a, b, c, d)
+                    ip_num = a*256**3 + b*256**2 + c*256 + d
+                    f.write(ip_addr + "," + str(ip_num) + "\n")
+    f.close()
 
 num_of_processes = cpu_count()-1
 chunk_factor = 1
@@ -23,12 +23,12 @@ print pool
 left = 256
 step = left / (num_of_processes * chunk_factor)
 while left > step:
-	high = left
-	left -= step
-	low = left
-	pool.apply_async(generate_ipv4, args=(low, high))
+    high = left
+    left -= step
+    low = left
+    pool.apply_async(generate_ipv4, args=(low, high))
 else:
-	pool.apply_async(generate_ipv4, args=(0, left))
+    pool.apply_async(generate_ipv4, args=(0, left))
 
 pool.close()
 pool.join()
