54da953f329ec74224efc52240fb511d5163391d
hvr <havran.init0@gmail.com>
2018-05-29 22:45:07 +0200

    exclamation mark typo in bash shell

--- 03_neo4j-import.sh
+++ 03_neo4j-import.sh
@@ -1,4 +1,3 @@
-#1/bin/sh
-
+#!/bin/bash
 
 neo4j-admin import --database=ipv4.db --nodes "file-0_4.csv,file-112_148.csv,file-148_184.csv,file-184_220.csv,file-220_256.csv,subnets-30000.csv" --relationships relationships-30000.csv
